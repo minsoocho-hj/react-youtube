342ccd5ca2abfc3b2d342f6ea9d067b76b585144
minsoo cho <khg04170@gmail.com>
2022-04-08 16:37:18 +0100

    🎨 Add youtube module in order to seperate network logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app";
+import Youtube from "./services/youtube";
 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

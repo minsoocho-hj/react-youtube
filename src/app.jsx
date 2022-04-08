07d322f2515756a7f4231695c25352066ce91656
minsoo cho <khg04170@gmail.com>
2022-04-08 16:36:43 +0100

    🙈 add .env

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,7 @@ import VideoList from "./components/video_list/video_list";
 import styles from "./app.module.css";
 import ShowVideo from "./components/show_video/show_video";
 
-function App() {
+function App({ youtube }) {
   const [videoList, setVideoList] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
@@ -15,58 +15,38 @@ function App() {
   };
 
   const searchVideo = (query) => {
-    setSelectedVideo(null);
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}r&key=AIzaSyDoifx4fcdLl7xMHxIhje4Z5t0QwAE77Zk`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((video) => setVideoList(video))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(query) //
+      .then((video) => {
+        setVideoList(video);
+        setSelectedVideo(null);
+      });
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideoList(videos));
+  }, [youtube]);
 
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDoifx4fcdLl7xMHxIhje4Z5t0QwAE77Zk",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideoList(result.items))
-      .catch((error) => console.log("error", error));
-  }, []);
   return (
-    <>
-      <div className={styles.app}>
-        <SearchHeader searchVideo={searchVideo} />
-        <div className={styles.content}>
-          {selectedVideo && (
-            <div className={styles.detail}>
-              <ShowVideo selectedVideo={selectedVideo} />
-            </div>
-          )}
-          <div className={styles.list}>
-            <VideoList
-              videoList={videoList}
-              selectVideo={selectVideo}
-              display={selectedVideo ? "list" : "grid"}
-            />
+    <div className={styles.app}>
+      <SearchHeader searchVideo={searchVideo} />
+      <div className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <ShowVideo selectedVideo={selectedVideo} />
           </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videoList={videoList}
+            selectVideo={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
